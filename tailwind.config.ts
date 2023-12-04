09c36d38e5a7af29dcc0c6582b7dd25dd98b6c36
Jacky Lo <jackylo.hc@gmail.com>
2023-12-04 16:58:23 +0800

    update packages, add new styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      maxWidth: {
+        '600': '600px',
+      },
+      colors: {
+        primary: '#006284',
+        secondary: '#99DCFF',
+        white: '#F1F1E6',
+        orange: '#ED982A',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
